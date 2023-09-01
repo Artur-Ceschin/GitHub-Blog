02675d82582747cd67f0b47a20abed097c82ed4f
Artur Ceschin <artur.ceschin@gmail.com>
2023-09-01 11:13:01 -0300

    feat: 🎸 card component used in the main page

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,32 @@
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+
+import { CardContainer } from './styles'
+import { formatRelativeTime } from '../../utils/formatRelativeTime'
+
+interface CardProps {
+  title: string
+  createdAt: Date
+  body: string
+  number: number
+}
+
+export function Card({ title, createdAt, body, number }: CardProps) {
+  const truncatedText = truncateText(body, 200)
+  const formattedDate = formatRelativeTime(createdAt.toString())
+
+  return (
+    <CardContainer href={`/post/${number}`}>
+      <header>
+        <h2>{title}</h2>
+        <span>{formattedDate}</span>
+      </header>
+
+      <ReactMarkdown>{truncatedText}</ReactMarkdown>
+    </CardContainer>
+  )
+}
+
+function truncateText(text: string, maxLength: number) {
+  if (text.length < maxLength) return text
+  return text.slice(0, maxLength) + '...'
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,45 @@
+import { styled } from 'styled-components'
+
+export const CardContainer = styled.a`
+  color: inherit;
+  text-decoration: inherit;
+
+  width: 100%;
+  height: 100%;
+  max-width: 416px;
+  max-height: 260px;
+  padding: 2rem;
+  cursor: pointer;
+  position: relative;
+
+  border-radius: 10px;
+  background-color: ${(props) => props.theme['base-post']};
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    margin-bottom: 20px;
+
+    h2 {
+      font-size: 20px;
+      line-height: 160%;
+      font-style: normal;
+      font-weight: 700;
+      max-width: 283px;
+
+      color: ${(props) => props.theme['base-title']};
+    }
+
+    span {
+      line-height: 160%;
+      font-size: 14px;
+      color: ${(props) => props.theme['base-span']};
+    }
+  }
+
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform: translateY(-10px);
+  }
+`
