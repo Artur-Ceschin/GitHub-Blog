6d91c043240458fb8081292cb763aaa23eb4c850
Artur Ceschin <artur.ceschin@gmail.com>
2023-09-01 11:12:23 -0300

    feat: 🎸 react router dom initial configuration

---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { DefaultLayout } from './layouts/DefaultLayout/indext'
+import { Blog } from './pages/Blog'
+import { Post } from './pages/Post'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Blog />} />
+        <Route path="/post/:postId" element={<Post />} />
+      </Route>
+    </Routes>
+  )
+}
