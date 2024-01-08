52161edcb546502b4aee3fecfc6e1364a43dcfb0
Artur Ceschin <artur.ceschin@gmail.com>
2024-01-08 16:39:02 -0300

    styles: added missing styles to project profile

---
+++ src/components/Header/Profile/styles.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components'
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 148px;
+    height: 148px;
+    flex-shrink: 0;
+
+    border-radius: 8px;
+  }
+`
